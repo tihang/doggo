485630c915bf38c5d727e2fb540d72af370b53f4
tihangrai <tihangrai@gmail.com>
2020-03-20 21:51:42 -0400

    nav refactors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
     def nav_link(text, path, method)
         if current_page?(path)
-             content_tag(:li, class: "nav-items active") do
+             content_tag(:li, class: "nav-items mx-2 active") do
                  link_to(path, class: "nav-link text-info bold", method: method) do
                      sanitize(text) + content_tag(:span, "(current)", class: "sr-only")
                  end
              end
          else
-             content_tag(:li, class: "nav-items") do
+             content_tag(:li, class: "nav-items mx-2") do
                  link_to(text, path, class: "nav-link", method: method)
              end
         end   
