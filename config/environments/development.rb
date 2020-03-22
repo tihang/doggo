547128e4348f61a6b88cf7cf1e525bf725c54536
tihangrai <tihangrai@gmail.com>
2020-03-22 18:02:57 -0400

    mailgun settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,11 +41,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              ENV['MAILGUN_SMTP_SERVER'], 
-  port:                 ENV['MAILGUN_SMTP_PORT'],
+  port:                 587,
   domain:               'my-app.herokuapp.com',
   user_name:            ENV['MAILGUN_SMTP_LOGIN'],
   password:             ENV['MAILGUN_SMTP_PASSWORD'],
   authentication:       'plain',
+  enable_starttls_auto: true
   }
 
   # Print deprecation notices to the Rails logger.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,11 +65,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              ENV['MAILGUN_SMTP_SERVER'],
-  port:                 ENV['MAILGUN_SMTP_PORT'],
+  port:                 587,
   domain:               'doggo-web.herokuapp.com',
   user_name:            ENV['MAILGUN_SMTP_LOGIN'],
   password:             ENV['MAILGUN_SMTP_PASSWORD'],
   authentication:       'plain',
+  enable_starttls_auto: true
   }
 
 
