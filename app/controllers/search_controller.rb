8cd8b9366659f03ab7a8d4a04fd90724d6fcd60a
tihangrai <tihangrai@gmail.com>
2020-03-22 17:28:16 -0400

    mailgun added

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
 
     def index
-        @posts = Post.all
+        @posts = Post.all.order("posts.created_at DESC")
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
