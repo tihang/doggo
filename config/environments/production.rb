f4af5c472fcf67275f1a4377cdb1531a8b8ef3de
tihangrai <tihangrai@gmail.com>
2020-03-23 13:22:40 -0400

    changed mailer to sendinblue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'https://doggo-web.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port:                 25,
+  address: 'smtp-relay.sendinblue.com',
+  port:                 587,
   domain:               'heroku.com',
   user_name:            ENV['SENDGRID_USERNAME'],
   password:             ENV['SENDGRID_PASSWORD'],
