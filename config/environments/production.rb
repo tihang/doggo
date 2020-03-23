2c971a51d1b1b6da3aeb8a3e4ce49c4769e0667b
tihangrai <tihangrai@gmail.com>
2020-03-22 20:32:01 -0400

    mailgun settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,8 +64,8 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port:                 25,
+  address: 'smtp.mailgun.org',
+  port:                 587,
   domain:               'heroku.com',
   user_name:            ENV['MAILGUN_SMTP_LOGIN'],
   password:             ENV['MAILGUN_SMTP_PASSWORD'],
