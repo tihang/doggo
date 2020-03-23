e5b3e66ebb29c370d938cb7af2742e4124cbf99a
tihangrai <tihangrai@gmail.com>
2020-03-23 13:58:28 -0400

    smpt port changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:             'smtp.gmail.com',
-  port:                 587,
+  port:                 465,
   domain:               'heroku.com',
   user_name:            ENV['SENDGRID_USERNAME'],
   password:             ENV['SENDGRID_PASSWORD'],
